ef1922ee17f7de43010129c6b19bc9a6202bfef0
Luis982210 <46784371+Luis982210@users.noreply.github.com>
2020-01-24 10:44:27 -0600

    Instancias

---
+++ Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab.Models
+{
+    public class Movie
+    {
+        public string name { get; set; }
+        public int year { get; set; }
+        public string director { get; set; }
+    }
+}
---
+++ MovieSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab.Models
+{
+    public class MovieSettings : IMovieSettings
+    {
+        public Stack<Movie> Movies { get; set; }
+        public MovieSettings()
+        {
+            Movies = new Stack<Movie>();
+        }
+        public void Create(Movie movie)
+        {
+            Movies.Push(movie);
+        }
+        public List<Movie> Get()
+        {
+            List<Movie> temp = new List<Movie>();
+            int cont = 1;
+            foreach (var item in Movies)
+            {
+                temp.Add(Movies.Peek());
+                cont++;
+                if (cont > 10)
+                {
+                    break;
+                }
+            }
+            return temp;
+        }
+    }
+    public interface IMovieSettings
+    {
+        Stack<Movie> Movies { get; set; }
+        void Create(Movie movie);
+        List<Movie> Get();
+    }
+}
